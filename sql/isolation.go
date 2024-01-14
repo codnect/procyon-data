353a91a0f2eb46849e99db8fd507ef5e569f0149
Burak Koken <burakkokenn@gmail.com>
2024-01-15 01:08:19 +0300

    Move sql package from codnect.io/procyon to codnect.io/procyon-data

---
+++ sql/isolation.go
@@ -0,0 +1,14 @@
+package sql
+
+type IsolationLevel int
+
+const (
+	LevelDefault IsolationLevel = iota
+	LevelReadUncommitted
+	LevelReadCommitted
+	LevelWriteCommitted
+	LevelRepeatableRead
+	LevelSnapshot
+	LevelSerializable
+	LevelLinearizable
+)
